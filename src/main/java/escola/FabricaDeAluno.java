4b07f335d9d90bdff06110df1f4fa91dbbd9112c
RaphaelVRossi <raphael.vieira.rossi@gmail.com>
2021-12-15 00:59:26 -0300

    aula 8 - factories

---
+++ src/main/java/escola/FabricaDeAluno.java
@@ -0,0 +1,27 @@
+package escola;
+
+/**
+ * FabricaDeAluno
+ * <p>
+ * Created at 15/12/2021.
+ *
+ * @author <a href="mailto:raphael.rossi@engdb.com.br">Raphael Rossi</a>
+ */
+public class FabricaDeAluno {
+
+    private Aluno aluno;
+
+    public FabricaDeAluno comNomeCpfEmail(String nome, String cpf, String email) {
+        this.aluno = new Aluno(new CPF(cpf), nome, new Email(email));
+        return this;
+    }
+
+    public FabricaDeAluno comTelefone(String ddd, String numero) {
+        this.aluno.adicionarTelefone(ddd, numero);
+        return this;
+    }
+
+    public Aluno criar() {
+        return this.aluno;
+    }
+}
