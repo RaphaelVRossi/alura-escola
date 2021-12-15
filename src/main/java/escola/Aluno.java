abb235e252b1f50d80fe76dcc1a764eb4dd066b9
RaphaelVRossi <raphael.vieira.rossi@gmail.com>
2021-12-14 23:45:32 -0300

    aula 01 - entidades

---
+++ src/main/java/escola/Aluno.java
@@ -0,0 +1,16 @@
+package escola;
+
+/**
+ * Aluno
+ * <p>
+ * Created at 14/12/2021.
+ *
+ * @author <a href="mailto:raphael.rossi@engdb.com.br">Raphael Rossi</a>
+ */
+public class Aluno {
+
+    private String cpf;
+    private String nome;
+    private String email;
+
+}
